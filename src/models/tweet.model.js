4a9ebac8170c1725c3af530febd99e922856158f
Sudeep Prajapati <sudeeppprajapati@gmail.com>
2024-10-05 17:38:31 +0530

    Add comment, like, playlist and tweet models

---
+++ src/models/tweet.model.js
@@ -0,0 +1,14 @@
+import { model, Schema } from "mongoose";
+
+const tweetSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+}, { timestamps: true })
+
+export const Tweet = model("Tweet", tweetSchema)
